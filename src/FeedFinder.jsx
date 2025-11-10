3879397f203a5872104f4b7835c93c3bb8de3259
IamBryanlim <bryanlimjh2009@GMAIL.COM>
2025-11-10 20:15:38 +0800

    test 24

--- src/FeedFinder.jsx
+++ src/FeedFinder.jsx
@@ -389,6 +389,7 @@ const FeedFinder = () => {
                      key={post.id}
                      post={post}
                      setShowRatingModal={setShowRatingModal}
+                     currentUserId={currentUserId} 
                      isLoggedIn={isLoggedIn}
                      isPremium={isPremium}
                      onAuthorClick={() => {
